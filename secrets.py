66cc1424ce053f216f8ff529dc1b88473dcd87a5
Alice Pote <alice.writes.wrongs@gmail.com>
2015-07-26 21:03:30 -0400

    Options! Error checking! Good stuff!

--- secrets.py
+++ secrets.py
@@ -1,3 +1,28 @@
-import gnupg
+from notes import Notes, firstrun
+import notes
+import os
+import sys
 
-gpg = gnupg.GPG(gnupghome='~/.gnupg')
+# intialization control flow
+if not os.path.isfile(os.environ['HOME'] + "/.secrets/config"):
+    if len(sys.argv) > 1:
+        if (sys.argv[1] == "init"):
+            firstrun()
+    else:
+        print("Secrets is not initialized. Pass 'init' to set up")
+# the rest of the options
+else:
+    notes = Notes()
+
+    if len(sys.argv) > 1:
+        if (sys.argv[1] == "init"):
+            print("Error: already set up")
+        elif (sys.argv[1] == "list"):
+            notes.listnotes()
+        elif (sys.argv[1] == "new"):
+            if len(sys.argv) > 2:
+                notes.newnote(sys.argv[2])
+            else:
+                print("Error: give it a title!")
+
+
