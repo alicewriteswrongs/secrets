4efab0761a0e4753323c47e17eced5e1fc2f8a75
Alice Pote <alice.writes.wrongs@gmail.com>
2015-07-28 13:46:11 -0400

    Added some ascii art!

--- secrets.py
+++ secrets.py
@@ -1,6 +1,16 @@
+#!/usr/bin/python3
 from notes import Notes, firstrun
 import os
 import sys
+
+## important stuff
+secrets ="""  _________                            __          
+ /   _____/ ____   ___________   _____/  |_  ______
+ \_____  \_/ __ \_/ ___\_  __ \_/ __ \   __\/  ___/
+ /        \  ___/\  \___|  | \/\  ___/|  |  \___ \ 
+/_______  /\___  >\___  >__|    \___  >__| /____  >
+        \/     \/     \/            \/          \/ 
+"""
 
 # intialization control flow
 if not os.path.isfile(os.environ['HOME'] + "/.secrets/config"):
@@ -15,8 +25,11 @@ else:
 
     if len(sys.argv) > 1:
         if (sys.argv[1] == "init"):
-            print("Error: already set up")
+            print(secrets)
+            print("\t\tis already set up!")
         elif (sys.argv[1] == "list"):
+            print("\tyour")
+            print(secrets)
             notes.listnotes()
         elif (sys.argv[1] == "new"):
             if len(sys.argv) > 2:
