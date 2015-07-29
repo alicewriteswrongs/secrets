94f9671d5924cd54c1a1148f8e159ec9d569f575
Alice Pote <alice.writes.wrongs@gmail.com>
2015-07-29 14:26:34 -0400

    Main function

--- secrets.py
+++ secrets.py
@@ -13,45 +13,47 @@ secrets ="""  _________                            __
 """
 
 # intialization control flow
-if not os.path.isfile(os.environ['HOME'] + "/.secrets/config"):
-    if len(sys.argv) > 1:
-        if (sys.argv[1] == "init"):
-            print("\twelcome to")
-            print(secrets)
-            firstrun()
-    else:
-        print(secrets)
-        print("\t\tis not initialized. Pass 'init' to set up")
-# the rest of the options
-else:
-    notes = Notes()
-
-    if len(sys.argv) > 1:
-        if (sys.argv[1] == "init"):
-            print(secrets)
-            print("\t\tis already set up!")
-        elif (sys.argv[1] == "list"):
-            print("\tyour")
-            print(secrets)
-            notes.listnotes()
-        elif (sys.argv[1] == "new"):
-            if len(sys.argv) > 2:
-                notes.newnote(sys.argv[2])
-            else:
-                print("Error: give it a title!")
-        elif (sys.argv[1] == "echo"):
-            if len(sys.argv) > 2:
-                notes.echonote(sys.argv[2])
-        elif (sys.argv[1] == "delete"):
-            if len(sys.argv) > 2:
-                notes.deletenote(sys.argv[2])
+def main():
+    if not os.path.isfile(os.environ['HOME'] + "/.secrets/config"):
+        if len(sys.argv) > 1:
+            if (sys.argv[1] == "init"):
+                print("\twelcome to")
+                print(secrets)
+                firstrun()
         else:
-            if (sys.argv[1] in notes.notes.keys()):
-                notes.editnote(sys.argv[1])
+            print(secrets)
+            print("\t\tis not initialized. Pass 'init' to set up")
+    # the rest of the options
     else:
-        print(secrets)
-        print("\ttry secrets.py list")
-        print("\tor secrets.py foo")
-    notes.close()
+        notes = Notes()
 
+        if len(sys.argv) > 1:
+            if (sys.argv[1] == "init"):
+                print(secrets)
+                print("\t\tis already set up!")
+            elif (sys.argv[1] == "list"):
+                print("\tyour")
+                print(secrets)
+                notes.listnotes()
+            elif (sys.argv[1] == "new"):
+                if len(sys.argv) > 2:
+                    notes.newnote(sys.argv[2])
+                else:
+                    print("Error: give it a title!")
+            elif (sys.argv[1] == "echo"):
+                if len(sys.argv) > 2:
+                    notes.echonote(sys.argv[2])
+            elif (sys.argv[1] == "delete"):
+                if len(sys.argv) > 2:
+                    notes.deletenote(sys.argv[2])
+            else:
+                if (sys.argv[1] in notes.notes.keys()):
+                    notes.editnote(sys.argv[1])
+        else:
+            print(secrets)
+            print("\ttry secrets.py list")
+            print("\tor secrets.py foo")
+        notes.close()
 
+if __name__ == '__main__':
+    main()
