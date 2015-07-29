7293e216703bb229303feef7a229f6ead3ea3579
Alice Pote <alice.writes.wrongs@gmail.com>
2015-07-29 13:43:48 -0400

    Can build debian package! woo!

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
         name = 'secrets',
         version = '0.1',
-        packages = ['.'],
+        py_modules = ['secrets', 'notes'],
+        test_suite = "tests",
         license = 'GPLv3',
         long_description = open('README.md').read(),
+        maintainer = 'Alice Pote',
+        maintainer_email = 'alice.writes.wrongs@gmail.com',
+        description = 'store your secrets using GPG!',
 )
+
