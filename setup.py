9be17439dd35f77affc8f8f7492179b884a6d729
Alice Pote <alice.writes.wrongs@gmail.com>
2015-07-29 12:00:10 -0400

    Making progress!

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+        name = 'secrets',
+        version = '0.1',
+        packages = ['.'],
+        license = 'GPLv3',
+        long_description = open('README.md').read(),
+)
